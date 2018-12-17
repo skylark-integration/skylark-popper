aa78a8d866a03889a9b5966a3d1afb3c7cff8535
hudaokeji <github@hudaokeji.com>
2018-12-18 07:18:25 +0900

    init commit

---
+++ dist/skylark-ui-popper/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-ui-popper - The skylark popup&tooltip ui plugin.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylarkui/skylark-ui-popper/
+ * @license MIT
+ */
+define(["./Popper"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-ui-popper/main.js
@@ -0,0 +1,5 @@
+define([
+	"./Popper"
+],function(Popper){
+	return Popper;
+});
